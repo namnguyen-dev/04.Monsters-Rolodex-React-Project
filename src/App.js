672be8717dff8231c637e633b83f35cfe0e65689
Nam Nguyen <nguyenthanhnamjuly27th1990@gmail.com>
2021-03-16 16:53:59 +0700

    Adding card, serach-box components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     };
   }
 
   componentDidMount() {
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(res => res.json())
-  .then(users =>this.setState({ monsters: users}))
-}
-
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({ monsters: users }));
+  }
 
   render() {
-  return ( 
-  <div className="App">
-  <CardList name="Nam">
-  {
-     this.state.monsters.map(monster => (<h1 key={monster.id}>{monster.name}</h1>))
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <SearchBox
+          placeholder="search monster"
+          handleChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
   }
-  </CardList >
-  
-  </div>
-  )
-}
 }
 
 export default App;
-  
